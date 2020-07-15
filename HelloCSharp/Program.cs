3d069f874f1132ee95cdc76990832f521a3a9e35
javyb92 <javier.a.barragan92@gmail.com>
2020-07-15 15:31:29 -0500

    added csharp calculator

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
 namespace HelloCSharp
+
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Enter first name: ");
+            string inputA = Console.ReadLine(); //eager, datatype is explicit prior to content of Readline
+            Console.Write("Enter last name: ");
+            var inputB = Console.ReadLine(); //lazy, datatype is given based oncontent of ReadLine
+
+            Console.WriteLine("Yo " + inputA + " " + inputB + ", how are you?");
+            Console.WriteLine("{0} {1}", inputA, inputB);
+            Console.WriteLine($"{inputA} {inputB}");
+
+            Console.ReadLine();
+
         }
     }
 }
